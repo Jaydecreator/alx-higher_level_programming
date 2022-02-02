aeb268cd0fdc8166b7dddb33a37ae0731b67d45a
Jay Njabz De Creator <ndlovunjabulo17@gmail.com>
2022-02-02 13:08:25 +0000

    #0 and o#1 basic scripts to show Database and create database

--- 0x0D-SQL_introduction/0-list_databases.sql
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -1,3 +1,4 @@
 --the script will display all Databases
 --this queiry will display all databases
 SHOW DATABASES;
+
--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,5 +1,4 @@
-/*
-when the script is ran, a new database will be created in mysql
-with this query a new Database is created
-*/
+-- when the script is ran, a new database will be created in mysql
+-- with this query a new Database is created
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
+
