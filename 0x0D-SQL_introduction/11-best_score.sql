0dfa0bb1215173c958720def27e9d5ea6334156e
Jay Njabz De Creator <ndlovunjabulo17@gmail.com>
2022-02-05 14:38:30 +0000

    redone and fixed

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,3 +1,3 @@
---
---
+-- listing all records with a score >= 10
+-- the query will list on the score >= 10 in descending order
 SELECT score, name FROM second_table ORDER BY score DESC WHERE score >= 10;
