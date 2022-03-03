34ffb1fa6d9b43d9b34af6d8ac9eac1b55ce01bc
Jay Njabz De Creator <ndlovunjabulo17@gmail.com>
2022-03-03 20:13:20 +0000

    fixed 2nd

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-// script prints 3 lines but by using an array of string and a loop
-const words = ['C is fun', 'Python is cool', 'Javascript is amazing']
-
-for (let i = 0; i < 3; i++){
-  console.log(words[i]);
+const phrases = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let i = 0;
+while (i < phrases.length) {
+  console.log(phrases[i]);
+  i++;
 }
